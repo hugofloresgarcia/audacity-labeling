7771262db1ed3b1688fda220a53764a9fb8dd070
hugo flores <hf01049@georgiasouthern.edu>
2020-11-02 10:50:32 -0600

    remove old models

--- weights/model/code/__torch__/torch/nn/modules/conv/___torch_mangle_0.py
+++
@@ -1,9 +0,0 @@
-class Conv1d(Module):
-  __parameters__ = ["weight", ]
-  __buffers__ = []
-  weight : Tensor
-  training : bool
-  def forward(self: __torch__.torch.nn.modules.conv.___torch_mangle_0.Conv1d,
-    input: Tensor) -> Tensor:
-    imag = torch._convolution(input, self.weight, None, [242], [1101], [1], False, [0], 1, False, False, True)
-    return imag
